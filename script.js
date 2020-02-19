306c618fc132590808f017e00dda1b47d8947682
codewithsrobins1 <codewithsrobins1@gmail.com>
2020-02-18 19:48:00 -0800

    021820

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Landing Page Description Switch
-let descriptions = ['front-end developer','builder', 'learner'];
+let descriptions = ['front-end developer','sports enthusiast', 'video-gamer'];
 let count = 0;
 const title = document.querySelector('#landingDesc');
 const timer = setInterval(titleChange, 2000);
